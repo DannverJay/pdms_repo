947364869d7c19db5269e58c99e0b8e049cfa4b7
DannverJay <dannverjaylagramada@student.laverdad.edu.ph>
2023-06-22 19:20:37 +0800

    Update

--- resources/views/admin/pages/personnel/personnel-profile.blade.php
+++ resources/views/admin/pages/personnel/personnel-profile.blade.php
@@ -16,8 +16,8 @@
                             </div>
                         </div>
                         <div class="nk-block-head-content">
-                            <a href="{{ route('view.personnel.profile', ['id' => $personnel->id]) }}" class="btn btn-outline-light bg-white d-none d-sm-inline-flex"><em class="icon ni ni-arrow-left"></em><span>Back</span></a>
-                            <a href="{{ route('view.personnel.profile', ['id' => $personnel->id]) }}" class="btn btn-icon btn-outline-light bg-white d-inline-flex d-sm-none"><em class="icon ni ni-arrow-left"></em></a>
+                            <a href="{{ route('personnel-list') }}" class="btn btn-outline-light bg-white d-none d-sm-inline-flex"><em class="icon ni ni-arrow-left"></em><span>Back</span></a>
+                            <a href="{{ route('personnel-list') }}" class="btn btn-icon btn-outline-light bg-white d-inline-flex d-sm-none"><em class="icon ni ni-arrow-left"></em></a>
                         </div>
                     </div>
                 </div><!-- .nk-block-head -->
